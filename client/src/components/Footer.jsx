159798017c58c8a1bb614bc23b476ad33fa83d16
Aakash Jain <aakashjainofficial@gmail.com>
2023-01-14 08:09:56 +0530

    --no-edit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -14,11 +14,9 @@ const Footer = () => {
             <h5>Get in Touch</h5>
 
             <div id='adB'>
-                <a href="https://in.linkedin.com/in/vjti-alumni-association-6b66b0105?challengeId=AQFAkq1JR331bwAAAXcANvweeO8">Mail</a>
-                <a href="">LinkedIn</a>
-                <a href="">Github</a>
+                <a href="https://in.linkedin.com/in/vjti-alumni-association-6b66b0105?challengeId=AQFAkq1JR331bwAAAXcANvweeO8">LinkedIn</a>
                 <a href="https://twitter.com/vjti_alumni?ref_src=twsrc%5Etfw%7Ctwcamp%5Eembeddedtimeline%7Ctwterm%5Eprofile%3">Twitter</a>
-            </div> 
+            </div>
         </div> 
    </div>
 </>
