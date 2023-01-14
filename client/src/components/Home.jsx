e8a34bb61c689ee7eae435effce86b4f130becb6
Aakash Jain <aakashjainofficial@gmail.com>
2023-01-14 07:37:38 +0530

    --no-message

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
     <>
     <div className='home' id='home'>
         <main>
-            <h1>TechyStar</h1>
-            <p>Solution to all your problems</p>
+            <h1>VJTI Alumni Association</h1>
+            <p>Making sure the legacy lives on</p>
         </main>
     </div>
 
@@ -19,18 +19,23 @@ const Home = () => {
     <div className="home2">
         <img src={vg} alt="Graphics" />
        <div>
-          <p>We are your one and only solution to the tech problems you face
-            every day. We are leading tech company whose aim is to increase the
-            problem solving ability in children.</p>
+          <p>They Say "Never Forget Your Roots"<br>
+          </br>
+          <br></br>
+          Ever lost touch with some of your batchmates and peers? Now reconnect with them and relive your college days! It's never too late to connect with peers who shared the same hunger for excellence as you.   
+          </p>
        </div>
     </div>
 
     <div className="home3" id='about'>
         <div>
-            <h1>Who we are?</h1>
+            <h1>Once a VJTIan, Always a VJTIan</h1>
             <p>
-            Tech problems are a common occurrence in today's world, where we rely heavily on technology in almost every aspect of our lives. From our smartphones and laptops to our home appliances and security systems, it seems like there is always some sort of tech problem lurking around the corner. These problems can range from simple issues like a slow internet connection or a glitchy app, to more serious problems like hardware failures or data breaches. No matter what the issue is, tech problems can be frustrating and time-consuming to resolve, and they can often disrupt our daily routines and productivity.
-            </p>
+            <br></br>
+            For 4+ years, the VJTI community has nurtured us.<br></br> Don't you think it is our turn now? If not us, who? If not now, when?
+            <br></br>
+            The Alumni meet is an event for old relationships to be cherished, celebrated, refreshed, and new ones to be formed 
+                            </p>
         </div>
     </div>
 
@@ -38,7 +43,7 @@ const Home = () => {
         <div>
             <h1>Brands</h1>
             <article>
-                
+
                 <div style={{animationDelay:"0.3s"}}>
                     <AiFillGoogleCircle />
                     <p>Google</p>
